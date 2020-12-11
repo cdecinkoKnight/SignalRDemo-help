a600e9110226ff78435307b4f9c059a00b06ad93
Connie DeCinko <connie.decinko@knighttrans.com>
2020-12-11 16:51:05 -0700

    still does not have a default constructor

--- SignalRDemo/Startup.cs
+++ SignalRDemo/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using Autofac.Integration.SignalR;
+using Autofac.Integration.WebApi;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
 using Microsoft.AspNet.SignalR.Infrastructure;
 using Microsoft.Owin;
 using Owin;
@@ -18,7 +20,9 @@ namespace SignalRDemo
             var builder = new ContainerBuilder();
             var config = new HubConfiguration();
 
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
             builder.RegisterHubs(Assembly.GetExecutingAssembly());
+            builder.RegisterType<DefaultAssemblyLocator>().As<IAssemblyLocator>();
             builder.RegisterType<AutofacDependencyResolver>().As<IDependencyResolver>().SingleInstance();
             builder.RegisterType<ConnectionManager>().As<IConnectionManager>().SingleInstance();
 
